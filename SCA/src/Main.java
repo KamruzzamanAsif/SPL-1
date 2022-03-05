1229acb83ae977d6e1ce4d3af2975064cf5399f0
Kamruzzaman Asif <72350695+KamruzzamanAsif@users.noreply.github.com>
2022-03-05 17:19:29 +0600

    testing

--- SCA/src/Main.java
+++ SCA/src/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[]args) throws IOException {
-        //String path = "G:/SCA/src/editDistance.cpp";
+
         String path = "G:/SCA/src/LOC test.c";
 
         LOC loc = new LOC();
