4d48d5d8a787f5407473bf145854ecb474580f46
Kamruzzaman Asif <72350695+KamruzzamanAsif@users.noreply.github.com>
2022-04-01 09:00:34 +0600

    SCA interface added

--- SCA_GUI/src/main/java/GUI/MainController.java
+++ SCA_GUI/src/main/java/GUI/MainController.java
@@ -1,14 +1,24 @@
 package GUI;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onSCAClick() {
-        welcomeText.setText("Welcome to SCA");
+    public void clickOnStart(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SCAInterface.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
